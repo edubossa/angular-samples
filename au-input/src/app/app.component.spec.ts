046c1d565b88046d493ba5d8741103fe35caf983
Eduardo Wallace <edubossa@gmail.com>
2019-03-22 16:22:52 -0300

    ajustes testes

--- au-input/src/app/app.component.spec.ts
+++ au-input/src/app/app.component.spec.ts
@@ -1,31 +1,46 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
+import { InputRefDirective } from './lib/common/input-ref.directive';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent,
+      fixture: ComponentFixture<AppComponent>,
+      el: DebugElement;
+
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, AuFaInputComponent, InputRefDirective
       ],
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    el = fixture.debugElement;
+  });
+
+  // TODO CONTINUAR AULA 26
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'au-input'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('au-input');
+    expect(component.title).toEqual('au-input');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to au-input!');
+  it('should create a font awesome email input', () => {
+    const emailField = el.query(By.css('#email-field'));
+    expect(emailField).toBeTruthy();
+    //expect(emailField.query(By.css('i.icon.fa.fa-envelop'))).toBeTruthy();
+    expect(emailField.query(By.css('input.test-class'))).toBeTruthy();
   });
+
 });
--- au-input/src/app/app.module.ts
+++ au-input/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
-import { InputRefDirective } from './lib/common/input-ref.directive';
+import { AuInputModule } from './lib/au-input.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AuFaInputComponent,
-    InputRefDirective
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
