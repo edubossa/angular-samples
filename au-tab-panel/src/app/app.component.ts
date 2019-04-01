10e27fa0d40b5cf8c25b82286d5a0f3fbec94892
Eduardo Wallace <edubossa@gmail.com>
2019-04-01 19:39:21 -0300

    add new module course advanced angular

---
+++ au-tab-panel/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// 038 Implementing The Tab Panel Component - Initial Version Up And Running - OK
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./tab-panel.component.scss', './app.component.scss']
+})
+export class AppComponent {
+
+}
